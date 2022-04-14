a0e3db44f4c5beda40f6c193ccfed5b48c9c4889
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-04-14 15:48:59 +0530

    completed the user home

--- client/src/components/Pages/Forum/Home/ForumCard/forumcard.js
+++ client/src/components/Pages/Forum/Home/ForumCard/forumcard.js
@@ -7,25 +7,44 @@ export default function forumcard() {
     <div>
      
         <div className='forumcard-mainbox'>
+
           <div className='forumcard-container'>
             <h4><b>Welcome to Developers & Designers Runtime Support Community's Forum</b></h4>
             <p>Welcome to free code camp's forum</p>
             <p>Please introduce yourself by replying to this thread with:)</p>
           </div>
+          
+          <span>
+            <h5>40</h5>
+            <i class="fa-solid fa-heart"></i>
+          </span>
+
           <div className='forumcard-category-box'>
             General
           </div>
+          
         </div>
 
+
+
+
         <div className='forumcard-mainbox'>
+
           <div className='forumcard-container'>
             <h4><b>Welcome to Developers & Designers Runtime Support Community's Forum</b></h4>
             <p>Welcome to free code camp's forum</p>
             <p>Please introduce yourself by replying to this thread with:)</p>
-            <div className='forumcard-category-box'>
-            General
-            </div>
           </div>
+          
+          <span>
+            <h5>40</h5>
+            <i class="fa-solid fa-heart"></i>
+          </span>
+          
+          <div className='forumcard-category-box'>
+            General
+          </div>
+          
         </div>
 
     </div>
--- client/src/components/Pages/Forum/Home/home.js
+++ client/src/components/Pages/Forum/Home/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../../Header/header'
 import ForumCard from '../Home/ForumCard/forumcard'
 import './home.css'
+import Footer from '../../../Footer/footer'
 
 export default function home() {
   return (
@@ -10,28 +11,40 @@ export default function home() {
         
 
         <div>
-          <button> Populer</button>
-          <button> Latest</button>
+          <form className="home-form-flex">
+            <input id="home-searchNav" type="search" placeholder="Search" aria-label="Search" /> 
+          </form>
         </div>
 
-        <button>
-          NEW TOPIC
-        </button>      
+        
+        <div className='home-btn-container'>
 
-        <div>
-          <h6>Topic</h6>
-          <h6>Reply</h6>
-          <hr/>
+          <div className='home-filter'>
+            <button className='home-filter-btn' onclick="filterSelection('popular')"> Popular</button>
+            <button className='home-filter-btn' onclick="filterSelection('latest')"> Latest</button>
+          </div>
+
+          <button className='home-new-btn'>
+            <i class="fa-solid fa-plus"></i>&nbsp;&nbsp;NEW TOPIC
+          </button>  
+           
+          <div className='home-topic-txt'>
+            <h6 className='home-topic-txt-topic'>Topic</h6>
+            <h6 className='home-topic-txt-reply'>Reply</h6>
+          </div>
+
         </div>
 
+
+        <br/>
+        <hr/>
+        
         <div className='home-card-container'>
           <ForumCard/>
         </div>
-        
-        
+    
 
-
-
+        <Footer/>
 
     </div>
     

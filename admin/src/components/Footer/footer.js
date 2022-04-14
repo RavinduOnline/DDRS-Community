f30cd46dc25955d5a82b997ed66f18ef805a3358
Ravindu Rasanga <it20189730@my.sliit.lk>
2022-04-14 12:05:50 +0530

    Admin Footer Implemented

--- admin/src/components/Footer/footer.js
+++ admin/src/components/Footer/footer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './footer.css'
 
 export default function footer() {
   return (
-    <div>footer</div>
+    <div>
+      
+      <footer>
+          <div className="footer-box" ><a href='/copyright'>© Copyright -<span>Team X</span> @SLIIT</a></div>
+      </footer>
+
+    </div>
   )
 }

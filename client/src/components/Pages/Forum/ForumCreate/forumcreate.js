e2f1f16e664f4aac5e2b4046b380409623cfcae5
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-04-14 16:25:29 +0530

    forum page setup and coding

--- client/src/components/Pages/Forum/ForumCreate/forumcreate.js
+++ client/src/components/Pages/Forum/ForumCreate/forumcreate.js
@@ -1,7 +1,52 @@
 import React from 'react'
+import Header from '../../../Header/header'
+import Footer from '../../../Footer/footer'
+import './forumcreate.css'
 
 export default function forumcreate() {
   return (
-    <div>forumcreate</div>
+    <div>
+      
+      <Header/>
+
+
+
+
+        <div className='forumcreate-topic'>
+          <h6 className='forumcreate-topic-txt'>Proxy does not work with the React build version</h6>
+          <p>Dec 13,2022 at 2:04</p>
+        </div>
+
+        <br/>
+        <hr/>
+
+        <div>
+          <p>I'm bit annoyed that the ENTSO-E only provides XML responses and no JSON formats for their API. Does anyone have experienced with converting text/xml responses to data frames in R? I usually use @data_life Sorry there was a typo in the lapps () definition, which I corrected. One shouldn't name variables with the same name as a function. "file" in this case. Sometimes, I should take my own advise :)-
+        </p>
+
+        <img
+className="header-logo"
+src={Logo}
+alt="DDRS Logo"
+loading="lazy">
+
+        </div>
+
+
+
+
+      <Footer/>
+
+    </div>
+
+    
+
   )
 }
+
+
+
+
+
+
+

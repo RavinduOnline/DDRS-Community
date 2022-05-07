2ce3b919b92fe708e8663c280055e7c49d1010cd
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-05-07 19:13:39 +0530

    complete the POST method

--- backend/models/forum.js
+++ backend/models/forum.js
@@ -1,35 +1,34 @@
 const mongoose = require("mongoose");
 
-const forumSchema = new mongoose.Schema({
-    Title:{
-        type:String,
-        trim: true,
-        required: true,
+const forumSchema = new mongoose.Schema(
+  {
+    Title: {
+      type: String,
+      trim: true,
+      required: true,
     },
-
     FCategory:{
-        type:String,
-        required: true,
-    },
-
-    Description:{
-        type:String,
-        trim: true,
-        required: true, 
-    },
-
-    Body:{
-        type:String,
+        type: String,
         trim: true,
         required: true,
+      },
+    Description: {
+      type: String,
+      trim: true,
+      required: true,
     },
-
-    Comment_id:{
-        type:String,
+    Body: {
+      type: String,
+      trim: true,
+      required: true,
     },
+    Created_at: {
+        type:Date,
+        default: Date.now()
+      },
 
-},
-{ timestamps: true }
+  },
+  { timestamps: true }
 );
 
 module.exports=mongoose.model("Forum",forumSchema);
--- backend/routes/forum.js
+++ backend/routes/forum.js
@@ -3,17 +3,16 @@ const mongoose=require('mongoose');
 const router = express.Router();
 const Forum = mongoose.model('Forum');
 
-router.get('/forumget',(req,res)=>{
-    res.send('Welcome to stackflow clone tutorial');
-})
+router.get('/forum', (req, res)=>{
+    res.send("Hi I'm Forum Get Method")
+  
+ });
 
-
-    router.post("/forumcreate", async (req, res) => {
-
-
-    const { Title, FCategory, Description,Body } = req.body;
-    try {
-        if(!Title || !FCategory || !Description || !Body ){
+// Create
+router.post("/forumcreate", async (req, res) => {
+    const {Title, FCategory, Description, Body } = req.body;
+  try {
+        if(!Title || !FCategory || !Description || !Body){
             return res.status(422).json({ error: "Please fill all the field" });
         }
 
@@ -23,13 +22,10 @@ router.get('/forumget',(req,res)=>{
             Description,
             Body,
          });
-        const newForumCreate = await newForum.save()
-        if(newForumCreate){
-            return res.status(201).json({ message: "forum created successfully" });
+        const forumCreated = await newForum.save()
+        if(forumCreated){
+            return res.status(201).json({ message: "User created successfully" });
         } 
-        else{
-            return res.status(201).json({ message: "forum created err" });
-        }
 
     } catch (err) {
         console.log(err);

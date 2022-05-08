fbbff475ba102b032b5eeccccced7210a9989cba
Janiya1999 <it20148454@my.sliit.lk>
2022-05-08 20:06:49 +0530

    reset password backend implemented

--- backend/routes/profileauth.js
+++ backend/routes/profileauth.js
@@ -102,17 +102,19 @@ router.get('/usersetting/:id',(req, res) => {
 
 // Reset password (Update)
 
-router.put('resetpassword/:id',(req, res)=>{
-
-    User.findByIdAndUpdate(req.params.id,
-
+router.put('/resetpassword/:id',(req, res)=>{
+    User.findByIdAndUpdate(
+    req.params.id,
+    {
+        $set:req.body
+    },
     (err,user)=>{
         if(err){
         return res.status(400).json({error:err});
     }
 
     return res.status(200).json({
-        user
+        success:"Updated Successfully"
         });
     }
 );

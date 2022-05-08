c01ba9dc8328720961c4222c215a6ddfe6780fd5
Sudhari Sandamini <IT20049218@my.sliit.lk>
2022-05-08 13:52:19 +0530

    create retriew method

--- backend/routes/forum.js
+++ backend/routes/forum.js
@@ -4,8 +4,7 @@ const router = express.Router();
 const Forum = mongoose.model('Forum');
 
 router.get('/forum', (req, res)=>{
-    res.send("Hi I'm Forum Get Method")
-  
+    res.send("Hi I'm Forum Get Method")  
  });
 
 // Create
@@ -33,5 +32,171 @@ router.post("/forumcreate", async (req, res) => {
   }
 });
 
+//retrieve
+router.get("/forumget", async (req, res) => {
+    const error = {
+      message: "Error in retrieving Forum",
+      error: "Bad request",
+    };
+  
+    Forum.aggregate([
+      {
+        $lookup: {
+          from: "comments",
+          let: { forum_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$forum_id", "$$forum_id"],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                // user_id: 1,
+                comment: 1,
+                created_at: 1,
+                // forum_id: 1,
+              },
+            },
+          ],
+          as: "comments",
+        },
+      },
+      {
+        $lookup: {
+          from: "reply",
+          let: { forum_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$forum_id", "$$forum_id"],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                // user_id: 1,
+                // reply: 1,
+                // created_at: 1,
+                // forum_id: 1,
+                // created_at: 1,
+              },
+            },
+          ],    
+          as: "replyDetails",
+        },
+      },
+      // {
+      //   $unwind: {
+      //     path: "$answerDetails",
+      //     preserveNullAndEmptyArrays: true,
+      //   },
+      // },
+      {
+        $project: {
+          __v: 0,
+          // _id: "$_id",
+          // answerDetails: { $first: "$answerDetails" },
+        },
+      },
+    ])
+      .exec()
+      .then((forumDetails) => {
+        res.status(200).send(forumDetails);
+      })
+      .catch((e) => {
+        console.log("Error: ", e);
+        res.status(400).send(error);
+      });
+  });
+  
+  router.get("forumget/:id", async (req, res) => {
+    try {
+      // const question = await QuestionDB.findOne({ _id: req.params.id });
+      // res.status(200).send(question);
+      Forum.aggregate([
+        {
+          $match: { _id: mongoose.Types.ObjectId(req.params.id) },
+        },
+        {
+          $lookup: {
+            from: "reply",
+            let: { forum_id: "$_id" },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ["$forum_id", "$$forum_id"],
+                  },
+                },
+              },
+
+              {
+                $project: {
+                  _id: 1,
+                  user: 1,
+                  reply: 1,
+                  // created_at: 1,
+                  forum_id: 1,
+                  Created_at: 1,
+                },
+              },
+            ],
+            as: "replyDetails",
+          },
+        },
+        {
+          $lookup: {
+            from: "comments",
+            let: { forum_id: "$_id" },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ["$forum_id", "$$forum_id"],
+                  },
+                },
+              },
+              {
+                $project: {
+                  _id: 1,
+                  forum_id: 1,
+                  user: 1,
+                  Comment: 1,
+                  // created_at: 1,
+                  // question_id: 1,
+                  Created_at: 1,
+                },
+              },
+            ],
+            as: "comments",
+          },
+        },
+        {
+          $project: {
+            __v: 0,
+          },
+        },
+      ])
+        .exec()
+        .then((forumDetails) => {
+          res.status(200).send(forumDetails);
+        })
+        .catch((e) => {
+          console.log("Error: ", e);
+          res.status(400).send(error);
+        });
+    } catch (err) {
+        console.log(err)
+      res.status(400).send({
+        message: "Forum not found",
+      });
+    }
+  });
 
 module.exports = router;

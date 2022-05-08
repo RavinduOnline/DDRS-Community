d7a2a33f67bf782c31d775a957f5fbe2529b322f
RavinduOnline <ravindurasanga99@gmail.com>
2022-05-08 10:23:53 +0530

    WordFilter CRUD implemented

--- backend/routes/adminmanage.js
+++ backend/routes/adminmanage.js
@@ -51,7 +51,7 @@ router.get('/admindashboard/countdata', async (req, res)=>{
 // *************** Admin Manage WordFiltering REST Methods **************
 
 //Create WordFilter
-router.post("/adminmanage/wordcreate", async (req, res) => {
+router.post("/adminmanage/wordfilter/create", async (req, res) => {
     const {word, wcategory} = req.body;
   try {
         if(!word || !wcategory){
@@ -79,10 +79,83 @@ router.post("/adminmanage/wordcreate", async (req, res) => {
 });
 
 
+//Get WordFilter
+router.get('/adminmanage/wordfilter', async (req,res)=>{
+    
+    try{
+        //Display the date in descending order in using commenting count
+        const wordfilterData = await WordFilter.find();
+            
+            return res.status(200).json({
+                success:true,
+                existingWordFilterData:wordfilterData
+            });
+
+    }catch{
+        return res.status(400).json({ error: "Can't Find the top forum data" });
+    }
+
+});
+
+
+//update wordfilter
+router.put('/adminmanage/wordfilter/update/:id', async (req,res)=>{
+
+    const {word, wcategory} = req.body;
+  try {
+        if(!word || !wcategory){
+            return res.status(422).json({ error: "Please fill all the field" });
+        }
+
+        const lowerCaseWord = word.toLowerCase();
+        let findWord = await WordFilter.findOne({ word:lowerCaseWord });
+        if (findWord) {
+          return res.status(400).json({ error: "This Word already exists" });
+        }
+
+        else{
+            WordFilter.findByIdAndUpdate(
+                req.params.id,{
+                    word:lowerCaseWord,
+                    wcategory,
+                },
+                (err,post)=>{
+                    if(err){
+                        return res.status(400).json({
+                            error:err 
+                        });
+                    }
+
+                    return res.status(200).json({
+                        success:"Word Filter Updated Successfully"
+                    });
+                }
+            );
+        }
+
+  }catch{
+
+  }
+});
+
+
+//delete wordfilter
+router.delete('/adminmanage/wordfilter/delete/:id',(req,res)=>{
+    WordFilter.findByIdAndRemove(req.params.id).exec((err,deletedWordfilter) =>{
+
+        if(err){
+            return res.status(400).json({
+                message:"Wordfilter Delete unsuccessfully" ,err
+            });
+        }
+
+        return res.status(200).json({
+            message:"Wordfilter Delete successfully",deletedWordfilter
+        });
+    });
+});  
+
 // *************** Admin Manage WordFiltering REST Methods End **************
-
-
-
 
 
 module.exports = router;
